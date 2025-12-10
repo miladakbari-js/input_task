99bbe16200d8529e2562b14bee76638ee2cebadd
Milad Akbari <milad.akbari.codevelop@gmail.com>
2025-12-10 09:14:18 +0330

    feat(ui): add text-input component, icons, and type definitions
    Introduce new text-input component, add email icon, and define related TypeScript types
    
    Related to #1

---
+++ src/components/text-input/text-input.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { Input } from '@/components/text-input/input';
+import { CustomInputProps } from './types.d';
+
+export const TextInput = React.forwardRef<HTMLInputElement, CustomInputProps>(
+  ({ label, error, startIcon, endIcon, className, ...props }, ref) => {
+    return (
+      <div className="flex flex-col space-y-1">
+        {label ? <label className="mb-4 text-sm font-medium text-gray-200">{label}</label> : null}
+
+        <div className="relative">
+          {startIcon && (
+            <div className="pointer-events-none absolute top-1/2 left-3 -translate-y-1/2">
+              {startIcon}
+            </div>
+          )}
+          <Input
+            ref={ref}
+            className={cn(
+              'mx-auto flex w-full max-w-md flex-col gap-1 p-2',
+              startIcon ? 'pl-10' : 'pl-3',
+              endIcon ? 'pr-10' : 'pr-3',
+              error ? 'border-rose-500 focus:ring-rose-500' : '',
+              className,
+            )}
+            {...props}
+          />
+
+          {endIcon && (
+            <span className="pointer-events-auto absolute top-1/2 right-3 -translate-y-1/2">
+              {endIcon}
+            </span>
+          )}
+        </div>
+        {error ? <p className="mt-1 text-sm text-red-500">{error}</p> : null}
+      </div>
+    );
+  },
+);
+
+TextInput.displayName = 'CustomInput';
---
+++ src/components/ui/types.d.ts
@@ -0,0 +1,5 @@
+import { type ReactNode } from 'react';
+export type TestIconProps = {
+  className?: string;
+  icon?: ReactNode;
+};
